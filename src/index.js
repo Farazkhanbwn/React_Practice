ecf9206cd1c86c653652f804d763a6a5832a8893
Faraz Khan <122208660+Farazkhanbwn@users.noreply.github.com>
2023-08-10 18:47:18 +0500

    Completed My Bootstrap Button

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 // import ReactDOM from 'react-dom/client';
+import Bootbtn from './component/bootstrap_btn';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Alert from './component/alert';
 
-// ReactDOM.render(<App/> , document.getElementById("root"))
-ReactDOM.render(<Alert color = "warning"/> , document.getElementById("root"));
-// const root = ReactDOM.render(document.getElementById('root'));
-// ReactDOM.render(<Alert /> , document.getElementById("root"));
+// ReactDOM.render(<App/> , document.getElementById("btn"));
+// ReactDOM.render(<Alert color = "warning"/> , document.getElementById("root"));
+
+// direct render of Bootbtn
+ReactDOM.render(<Bootbtn type = "primary" text = "read more"/> , document.getElementById("btn"));
 
 // ReactDOM.render(alert , document.getElementById("root"));
 
