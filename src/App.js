5dfe0a02e4abd8e856c44a149c4053b9f62fed14
Faraz Khan <122208660+Farazkhanbwn@users.noreply.github.com>
2023-08-16 16:52:38 +0500

    Completed My Cusom Button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Bootbtn from "./component/bootstrap_btn";
+import CustomButton from "./component/custom-button/custom-button";
 import CustomInput from "./component/custom-input/custom-input";
 import { useState } from "react";
 
@@ -9,15 +10,19 @@ function App() {
     setState(text);
   };
 
+  let logMessage = () =>{ 
+    console.log("Welcome to Bootstrap Button");
+  }
   return (
     <div>
       {/* <Bootbtn text = "View More" type = "secondary"/> */}
-      <CustomInput
+      {/* <CustomInput
         type="text"
         placeholder="test"
         onChange={onChangeCustomInput}
       />
-      <p>value from custom input is: {state}</p>
+      <p>value from custom input is: {state}</p> */}
+      <CustomButton type ="btn-primary" title = "primary" logMessage= {logMessage}/>
     </div>
   );
 }
